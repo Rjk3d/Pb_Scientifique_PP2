8de0f48c4eb3cdcb653ed683a161ba94911d8faf
Rjk3d <max.sempels@gmail.fr>
2026-04-03 16:28:37 +0200

    Debut_Objectif_2_non_rempli

---
+++ psi_objectif1_groupePP2/TourneeFutee/Graph.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TourneeFutee
+{
+    public class Graph
+    {
+        public Matrix matAdjacence;
+        public Dictionary<string, int> sommetsDico;
+        public List<float> valeursSommets;
+        public bool estOriente;
+        public float valeurPasDArc;
+
+        public Graph(bool directed, float noEdgeValue = 0)
+        {
+            estOriente = directed;
+            valeurPasDArc = noEdgeValue;
+            matAdjacence = new Matrix(0, 0, noEdgeValue);
+            sommetsDico = new Dictionary<string, int>();
+            valeursSommets = new List<float>();
+        }
+
+        public int Order
+        {
+            get { return sommetsDico.Count; }
+        }
+
+        public void AddVertex(string name, float value = 0)
+        {
+            if (sommetsDico.ContainsKey(name)) throw new ArgumentException();
+
+            int index = valeursSommets.Count;
+            sommetsDico.Add(name, index);
+            valeursSommets.Add(value);
+
+            matAdjacence.AddRow(index);
+            matAdjacence.AddColumn(index);
+        }
+
+        public void RemoveVertex(string name)
+        {
+            if (sommetsDico.ContainsKey(name) == false) throw new ArgumentException();
+
+            int indexASupprimer = sommetsDico[name];
+
+            matAdjacence.RemoveRow(indexASupprimer);
+            matAdjacence.RemoveColumn(indexASupprimer);
+
+            valeursSommets.RemoveAt(indexASupprimer);
+            sommetsDico.Remove(name);
+
+            List<string> cles = new List<string>(sommetsDico.Keys);
+            for (int k = 0; k < cles.Count; k++)
+            {
+                string cle = cles[k];
+                if (sommetsDico[cle] > indexASupprimer)
+                {
+                    sommetsDico[cle] = sommetsDico[cle] - 1;
+                }
+            }
+        }
+
+        public void AddEdge(string source, string dest, float weight = 1)
+        {
+            if (sommetsDico.ContainsKey(source) == false || sommetsDico.ContainsKey(dest) == false)
+                throw new ArgumentException();
+
+            int i = sommetsDico[source];
+            int j = sommetsDico[dest];
+
+            if (matAdjacence.GetValue(i, j) != valeurPasDArc) throw new ArgumentException();
+
+            matAdjacence.SetValue(i, j, weight);
+            if (estOriente == false)
+            {
+                matAdjacence.SetValue(j, i, weight);
+            }
+        }
+
+        public void RemoveEdge(string source, string dest)
+        {
+            if (sommetsDico.ContainsKey(source) == false || sommetsDico.ContainsKey(dest) == false)
+                throw new ArgumentException();
+
+            int i = sommetsDico[source];
+            int j = sommetsDico[dest];
+
+            if (matAdjacence.GetValue(i, j) == valeurPasDArc) throw new ArgumentException();
+
+            matAdjacence.SetValue(i, j, valeurPasDArc);
+            if (estOriente == false)
+            {
+                matAdjacence.SetValue(j, i, valeurPasDArc);
+            }
+        }
+
+        public float GetEdgeWeight(string source, string dest)
+        {
+            if (sommetsDico.ContainsKey(source) == false || sommetsDico.ContainsKey(dest) == false)
+                throw new ArgumentException();
+
+            int i = sommetsDico[source];
+            int j = sommetsDico[dest];
+
+            float poids = matAdjacence.GetValue(i, j);
+            if (poids == valeurPasDArc) throw new ArgumentException();
+
+            return poids;
+        }
+
+        public void SetEdgeWeight(string source, string dest, float weight)
+        {
+            if (sommetsDico.ContainsKey(source) == false || sommetsDico.ContainsKey(dest) == false)
+                throw new ArgumentException();
+
+            int i = sommetsDico[source];
+            int j = sommetsDico[dest];
+
+            if (matAdjacence.GetValue(i, j) == valeurPasDArc) throw new ArgumentException();
+
+            matAdjacence.SetValue(i, j, weight);
+            if (estOriente == false)
+            {
+                matAdjacence.SetValue(j, i, weight);
+            }
+        }
+
+        public List<string> GetNeighbors(string name)
+        {
+            if (sommetsDico.ContainsKey(name) == false) throw new ArgumentException();
+
+            int i = sommetsDico[name];
+            List<string> voisins = new List<string>();
+
+            for (int j = 0; j < matAdjacence.NbColumns; j++)
+            {
+                if (matAdjacence.GetValue(i, j) != valeurPasDArc)
+                {
+                    foreach (var entree in sommetsDico)
+                    {
+                        if (entree.Value == j)
+                        {
+                            voisins.Add(entree.Key);
+                            break;
+                        }
+                    }
+                }
+            }
+            return voisins;
+        }
+
+        public float GetVertexValue(string name)
+        {
+            if (sommetsDico.ContainsKey(name) == false) throw new ArgumentException();
+            return valeursSommets[sommetsDico[name]];
+        }
+
+        public void SetVertexValue(string name, float val)
+        {
+            if (sommetsDico.ContainsKey(name) == false) throw new ArgumentException();
+            valeursSommets[sommetsDico[name]] = val;
+        }
+    }
+}
---
+++ psi_objectif1_groupePP2/TourneeFutee/Matrix.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TourneeFutee
+{
+    public class Matrix
+    {
+        public List<List<float>> donnees;
+        public float valeurParDefaut;
+
+        public Matrix(int nbRows = 0, int nbColumns = 0, float defaultValue = 0)
+        {
+            if (nbRows < 0 || nbColumns < 0) throw new ArgumentOutOfRangeException();
+
+            valeurParDefaut = defaultValue;
+            donnees = new List<List<float>>();
+
+            for (int i = 0; i < nbRows; i++)
+            {
+                List<float> ligne = new List<float>();
+                for (int j = 0; j < nbColumns; j++)
+                {
+                    ligne.Add(valeurParDefaut);
+                }
+                donnees.Add(ligne);
+            }
+        }
+
+        public float DefaultValue
+        {
+            get { return valeurParDefaut; }
+        }
+
+        public int NbRows
+        {
+            get { return donnees.Count; }
+        }
+
+        public int NbColumns
+        {
+            get
+            {
+                if (donnees.Count > 0) return donnees[0].Count;
+                return 0;
+            }
+        }
+
+        public void AddRow(int i)
+        {
+            if (i < 0 || i > NbRows) throw new ArgumentOutOfRangeException();
+
+            List<float> nouvelleLigne = new List<float>();
+            int colonnes = NbColumns;
+            for (int j = 0; j < colonnes; j++)
+            {
+                nouvelleLigne.Add(valeurParDefaut);
+            }
+            donnees.Insert(i, nouvelleLigne);
+        }
+
+        public void AddColumn(int j)
+        {
+            if (j < 0 || j > NbColumns) throw new ArgumentOutOfRangeException();
+
+            for (int k = 0; k < donnees.Count; k++)
+            {
+                donnees[k].Insert(j, valeurParDefaut);
+            }
+        }
+
+        public void RemoveRow(int i)
+        {
+            if (i < 0 || i >= NbRows) throw new ArgumentOutOfRangeException();
+            donnees.RemoveAt(i);
+        }
+
+        public void RemoveColumn(int j)
+        {
+            if (j < 0 || j >= NbColumns) throw new ArgumentOutOfRangeException();
+            for (int k = 0; k < donnees.Count; k++)
+            {
+                donnees[k].RemoveAt(j);
+            }
+        }
+
+        public float GetValue(int i, int j)
+        {
+            if (i < 0 || i >= NbRows || j < 0 || j >= NbColumns) throw new ArgumentOutOfRangeException();
+            return donnees[i][j];
+        }
+
+        public void SetValue(int i, int j, float v)
+        {
+            if (i < 0 || i >= NbRows || j < 0 || j >= NbColumns) throw new ArgumentOutOfRangeException();
+            donnees[i][j] = v;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < NbRows; i++)
+            {
+                for (int j = 0; j < NbColumns; j++)
+                {
+                    Console.Write(donnees[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
